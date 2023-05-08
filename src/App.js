d84df750e16c80f16b6e0300ea7932a860480c1f
Raiyhan Bhuiyan <96577789+raiyhanbhuiyan17@users.noreply.github.com>
2023-05-08 00:41:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <h2>Simple Counter App</h2>
+      <h2>Counter App</h2>
 
       <Counter/>
     </div>
